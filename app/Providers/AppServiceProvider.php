f20c6bc7995acfdade834654ea1c31a237d7e093
Tamer Solieman <tamer.solieman@gmail.com>
2018-04-14 14:41:01 +0300

    Use View composer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use App\Category;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**

f82d58731abd17e6362abc90079db4856d3d76bd
Tamer Solieman <tamer.solieman@gmail.com>
2018-04-14 20:02:41 +0300

    Remove the user menu from frontend

--- resources/views/front/layout/master.blade.php
+++ resources/views/front/layout/master.blade.php
@@ -24,12 +24,12 @@
 			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			  </button>
-			  <div class="collapse navbar-collapse" id="navbarResponsive">
+			  {{-- <div class="collapse navbar-collapse" id="navbarResponsive">
 				  <ul class="navbar-nav ml-auto">
 				  </ul>
 					<li class="nav-link"><a href="">دخول</a></li>
 					<li class="nav-link"><a href="">حساب جديد</a></li>
-			  </div>
+			  </div> --}}
 			</div>
 		</nav>
 
